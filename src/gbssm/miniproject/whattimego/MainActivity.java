95e4e495da4e3fca969cb73f64eace0d618f7625
woosung <loftyws91@naver.com>
2015-08-09 12:30:20 +0900

    Revert "test"
    This reverts commit 7517fa3f62fa20da4416f42a6470b0382bd721c7.

--- src/gbssm/miniproject/whattimego/MainActivity.java
+++ src/gbssm/miniproject/whattimego/MainActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-// test
+
 public class MainActivity extends Activity {
 
 	@Override
